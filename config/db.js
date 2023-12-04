7c12d233a9e65934ce84e4027076e9c6f58d2441
ayaniegain <ayaniegain@gmail.com>
2023-12-04 20:51:38 +0530

    fix: env

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const colors = require("colors");
 ///connect db from
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_CONNECT);
+    const conn = await mongoose.connect(process.env.MONGO_CONNECT || "mongodb+srv://ayaniegain:ayancodejs@cluster0.xuaxvv3.mongodb.net/task");
     console.log(
       `Connected to mongo db ${conn.connection.host}`.bgMagenta.white
     );
